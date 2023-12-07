f4de482f2502c2336c3ce6a97c7b74ed5b0a7c91
VladimirOlishevsky <vova.olishevskij@inbox.ru>
2023-12-07 17:13:11 +0300

    update selenium test

--- src/test.js
+++ src/test.js
@@ -6,6 +6,8 @@ async function loginTest() {
     await driver.findElement(By.name("Username")).sendKeys("student");
     await driver.findElement(By.name("Password")).sendKeys("Password123");
     await driver.findElement(By.className("submit-button")).click();
+    await driver.findElement(By.xpath("//*[text()='Logged In Successfully']"));
+    await driver.sleep(5000);
   } catch(err) {
     console.log(err);
     console.log("test failed")
